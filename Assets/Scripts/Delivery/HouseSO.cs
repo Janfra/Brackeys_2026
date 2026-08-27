8d0676f0f0b0bc33e294b5be13daca6e6a01a311
Jan <janfrancoherrera@gmail.com>
2026-08-27 13:29:16 +0100

    remove suffix from file name

--- Assets/Scripts/Delivery/HouseSO.cs
+++ Assets/Scripts/Delivery/HouseSO.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 /// <summary>
 /// Enum-like objects used as delivery address of each house, each house should contain this and determines whether the package is intended for this house based on match.
 /// </summary>
-[CreateAssetMenu(fileName = "HouseSO", menuName = "Scriptable Objects/Delivery/House")]
+[CreateAssetMenu(fileName = "House", menuName = "Scriptable Objects/Delivery/House")]
 public class HouseSO : ScriptableObject
 {
     
